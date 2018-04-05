11e6136715a0431017eba11d1e3506bf9814ccdc
Matt Little <zone117x@gmail.com>
2018-04-05 10:50:12 -0700

    no message

--- bin/exec.js
+++ bin/exec.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const process = require('process');
 const App = require('./../lib/app.js');
 const reqCwd = require('req-cwd');
 const death = require('death');
@@ -12,16 +13,23 @@ death((signal, err) => app.cleanUp(err));
 
 (async () => {
 
+  let workingDir = process.cwd();
+
   try {
+
     await app.generateCoverageEnvironment();
     await app.instrumentTarget();
     await app.launchTestrpc();
     await app.runTestCommand();
     await app.generateReport();
+  }  
+  //catch (err) {
+  //  console.error(err);
+  //}
+  finally {    
+    process.chdir(workingDir);
   }
-  catch (err) {
-    log(err);
-  }
+
 
 })();
 
